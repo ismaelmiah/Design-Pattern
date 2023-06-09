4a3023b65fad0a54169048cb8c9fbd03258b8cb1
Ismael Miah <ismail27.dec@gmail.com>
2023-06-10 00:24:23 +0600

    Add Strategy Pattern Example

---
+++ DesignPatterns/StrategyPattern/MuteQuack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class MuteQuack : IQuackBehavior
+    {
+        public void PerformQuack()
+        {
+            // Do nothing - can't quack
+            Console.WriteLine("Can't quack");
+        }
+    }
+}
---
+++ DesignPatterns/StrategyPattern/Squeak.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Squeak : IQuackBehavior
+    {
+        public void PerformQuack()
+        {
+            //rubber duckie squeak
+            Console.WriteLine("Squeak squeak");
+        }
+    }
+}
