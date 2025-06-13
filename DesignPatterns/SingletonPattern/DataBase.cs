aa9649ae8f98434cf305658621cd16359623e19d
Ismael Miah <ismail27.dec@gmail.com>
2025-06-13 21:30:00 +0100

    Add singleton pattern

---
+++ DesignPatterns/SingletonPattern/DataBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class DataBase
+    {
+        public static DataBase Instance;
+
+        private DataBase()
+        {
+            // Private constructor to prevent instantiation from outside
+        }
+
+        public static DataBase GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new DataBase();
+            }
+            return Instance;
+        }
+    }
+}
---
+++ DesignPatterns/SingletonPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SingletonPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = DataBase.GetInstance();
+            var instance2 = DataBase.GetInstance();
+            Console.WriteLine(ReferenceEquals(instance, instance2)); // True
+
+        }
+    }
+}
